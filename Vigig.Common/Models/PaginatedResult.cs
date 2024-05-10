12bf16bd372f6a3b6b1c489983abdd6b466b045d
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-05-10 10:04:52 +0700

    [GigServiceManagement][Hai] add paginatedhelper

---
+++ Vigig.Common/Models/PaginatedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Vigig.Common.Models;
+
+public class PaginatedResult
+{
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public long LastPage { get; set; }
+    public bool IsLastPage { get; set; }
+    public long Total { get; set; }
+    public object? Items { get; set; }
+}
