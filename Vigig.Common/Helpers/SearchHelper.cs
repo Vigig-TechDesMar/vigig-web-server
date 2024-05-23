89f0d8e52ec76cd2db2f1c7edae61c6b264ecf97
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-05-23 14:36:40 +0700

    [Search][Hai] add helper for search and sort

---
+++ Vigig.Common/Helpers/SearchHelper.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Linq.Expressions;
+using Vigig.Common.Models;
+using Vigig.Service.Models.Common;
+
+namespace Vigig.Common.Helpers;
+
+public class SearchHelper
+{
+    public static IEnumerable<T> BuildSearchResult<T>(IEnumerable<T> src,SearchUsingGet  request)
+    {
+        if (!string.IsNullOrEmpty(request.SearchField))
+        {
+            src = src
+                .Where(p => ReflectionHelper.GetStringValueByName(typeof(T), request.SearchField, p).Contains(request.SearchValue ?? string.Empty, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+        }
+        if (!string.IsNullOrEmpty(request.SortField))
+        {
+            Expression<Func<T, object>> orderExpr = p => ReflectionHelper.GetValueByName(typeof(T), request.SortField, p);
+            src = request.Descending
+                ? src.OrderByDescending(orderExpr.Compile()).ToList()
+                : src.OrderBy(orderExpr.Compile()).ToList();
+        }
+
+        return src;
+    }
+}
---
+++ Vigig.Common/Models/SearchUsingGet.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Vigig.Common.Models;
+
+namespace Vigig.Service.Models.Common;
+
+public class SearchUsingGet 
+{
+    [Range(1,int.MaxValue)]
+    public int PageSize { get; set; }
+    [Range(1,int.MaxValue)]
+    public int PageIndex { get; set; }
+    public string? SearchField { get; set; }
+    public string? SearchValue { get; set; }
+    public string? SortField { get; set; }
+    public bool Descending { get; set; }
+}
