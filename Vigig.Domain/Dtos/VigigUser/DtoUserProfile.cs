2a628539526660427450377d2891bd32baeffcf0
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-06-13 03:44:53 +0700

    [UserProfile][Hai] add fields in user profile dto

--- Vigig.Domain/Dtos/VigigUser/DtoUserProfile.cs
+++ Vigig.Domain/Dtos/VigigUser/DtoUserProfile.cs
@@ -7,6 +7,10 @@ public class DtoUserProfile
     public string? Phone { get; set; }
     
     public string? Address { get; set; }
+
+    public string? Email { get; set; }
+    
+    public string? ProfileImage { get; set; }
     
     public Guid BuildingId { get; set; }
 }
